346dfdbde54bbf305a997d3d5077770393212df6
Juan Correa <juancorrea2@my.unt.edu>
2024-05-23 20:23:04 -0500

    Setup npm for development

--- webpack.common.js
+++ webpack.common.js
@@ -17,4 +17,20 @@ module.exports = {
       inject: false
     })
   ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  }
 };
