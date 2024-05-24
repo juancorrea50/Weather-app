ce8abb3a3650035e26873b633af16cfa73550d29
Juan Correa <juancorrea2@my.unt.edu>
2024-05-23 21:30:53 -0500

    Prep a returned json object for api usage

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
-import _ from 'lodash';
+import "./styles.css";
+
+function WeatherAPI(inpVal) {
+  //Weather API fetch
+  async function searchAPI(urlInp) {
+    const response = await fetch(urlInp,{mode: 'cors'});
+    response.json().then(function(response){
+      console.log(response);
+    })
+    
+  }
+  function retrieveWeatherData(inp) {
+    let url =
+      "https://api.weatherapi.com/v1/current.json?key=cc22eb12673f40d29d423134242205&q=";
+    if (inp) {
+      url += inp;
+    } else {
+      url += "london";
+    }
+    try {
+      searchAPI(url);
+    } catch (error) {
+      console.error(
+        `Search Error: ${error} \n There was an api error. Displaying London's weather information`
+      );
+      retrieveWeatherData("london");
+    }
+  }
+  return retrieveWeatherData(inpVal);
+}
 
 function component() {
-    const element = document.createElement('div');
-  
-    // Lodash now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+  const container = document.createElement("div");
+
+  return container;
+}
+
+document.body.appendChild(component());
+WeatherAPI('london');
---
+++ src/widgetDOM.js
@@ -0,0 +1,18 @@
+export default widgetDOM(){
+    const weatherDisplay = document.createElement('div');
+    const weatherLocation = document.createElement('h1');
+    const weatherTemperature = document.createElement('h2');
+    const weatherForecast = document.createElement('h4');
+
+    const citySearchContainer = document.createElement('div');
+    const citySearchForm = document.createElement('form');
+    const citySearchInp = document.createElement('input');
+    const citySearchBtn = document.createElement('button');
+
+    citySearchContainer.classList.add('search-container');
+    citySearchForm.classList.add('search-form');
+    citySearchInp.classList.add('input');
+
+
+    
+}
